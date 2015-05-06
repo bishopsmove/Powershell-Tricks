7ba00c7597dd6bcfefde775e55ed002c52f66095
Jim Ownby <bishop@ownby.net>
2015-05-06 14:52:12 -0400

    Initial files commit

---
+++ ChangeWindowsDefaultScreen.ps1
@@ -0,0 +1,15 @@
+﻿
+<#
+    This is a simple utility that helps you change your Windows default/lock screen. It really only works 
+    if you have more than one file in the backgrounds directory noted below. 
+#>
+
+$bgPath = 'C:\Windows\System32\oobe\INFO\backgrounds'
+$excludeFile = 'backgrounddefault.jpg'
+
+$pictures = {} 
+if(Test-Path $bgPath -IsValid){
+ $pictures = gci -Path $bgPath -Exclude $excludeFile
+}
+
+$pictures | % { $_.Name } | Out-GridView -OutputMode Single | % { Copy-Item -Path $bgPath\$_ -Destination $bgPath\$excludeFile }
---
+++ README.md
@@ -0,0 +1,17 @@
+PowerShell Tricks
+===================
+
+PowerShell Tricks is simply a few PowerShell scripts to assist Windows users common or off-beat utilities that modify their workstation.
+
+
+
+Quick start
+-----------
+
+Clone the repo, `git clone git://github.com/bishopsmove/PowerShell-Tricks.git`
+
+
+License
+---------
+
+Standard GPL
